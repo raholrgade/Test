a16fba2836150e3ea105aabca8522ddde65d306c
Dell <Dell@DESKTOP-N0QF96N>
2022-10-18 11:39:22 +0530

    this is mokito test

---
+++ src/com/test/Mokito.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class Mokito {
+	public static void main(String[] args) {
+		for (int i=0;i<=10;i++) {
+			System.out.println("mokito used for fake data");
+		}
+	}
+
+}
